1d6a8a909ceb839c11c6e82eb2f0cd9e4895c5a6
Leon.Melamud <leon.melamud@flywire.com>
2024-08-31 18:37:56 +0300

    move files

--- public/dataAccess.js
+++ public/dataAccess.js
@@ -2,10 +2,12 @@
 async function readCarData() {
   try {
     const timestamp = new Date().getTime();
-    const response = await fetch(`/data/cars.json?t=${timestamp}`);
+    const response = await fetch(`./data/cars.json?t=${timestamp}`);
+    
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
+    
     const data = await response.json();
     console.log('Fetched car data:', data);
     return data;
